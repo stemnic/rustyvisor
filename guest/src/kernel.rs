014eca08c3840801d8b85661c5fe90598da03bd6
Ole Sivert Aarhaug <ole.sivert@gmail.com>
2022-03-18 18:55:11 +0000

    Added timer testing to test-kernel

--- guest/src/kernel.rs
+++ guest/src/kernel.rs
@@ -21,8 +21,61 @@ pub extern "C" fn rust_entrypoint() -> ! {
 
     let call = sbi_call(EXTENSION_TIMER, 0x0, 0xdead, 0xbeef);
     println!("Sbi call error: {},  value: {}", call.error, call.value);
+    assert_eq!(call.error, 0);
+    
+    println!("Testing timer");
+    enable_timer_interrupts();
 
     loop {}
+}
+
+fn enable_timer_interrupts() {
+    
+    let mut sstatus:usize;
+    unsafe{
+        asm!(
+            "csrr {0}, sstatus", out(reg) sstatus, options(nostack)
+        )
+    }
+    let sie_mask = 1 << 1 as usize;
+    let result: usize = sstatus | sie_mask;
+    unsafe{
+        asm!(
+            "csrw sstatus, {0}", in(reg) result, options(nostack)
+        )
+    }
+
+
+    let mut sie:usize;
+    unsafe{
+        asm!(
+            "csrr {0}, sie", out(reg) sie, options(nostack)
+        )
+    }
+    let sie_mask = 1 << 5 as usize;
+    let result: usize = sie | sie_mask;
+    unsafe{
+        asm!(
+            "csrw sie, {0}", in(reg) result, options(nostack)
+        )
+    }
+
+}
+
+fn clear_timers() {
+    let mut sie:usize;
+    unsafe{
+        asm!(
+            "csrr {0}, sie", out(reg) sie, options(nostack)
+        )
+    }
+    let sie_mask = !(1 << 5) as usize;
+    let result: usize = sie & sie_mask;
+    unsafe{
+        asm!(
+            "csrw sie, {0}", in(reg) result, options(nostack)
+        )
+    }
 }
 
 fn sbi_call(extension: usize, function: usize, arg0: usize, arg1: usize) -> SbiRet {
@@ -48,11 +101,71 @@ pub fn init() -> Result<(), Error> {
     // init UART
     uart::Uart::new(memlayout::UART_BASE).init();
 
+    println!("trap set to: {:?}", &(trap as unsafe extern "C" fn()) );
+
+    unsafe{
+        asm!(
+            "csrw stvec, {0}", in(reg) (trap as unsafe extern "C" fn() as usize)
+        );
+    }
+    let mut stvec: usize;
+    unsafe{
+        asm!(
+            "csrr {0}, stvec", out(reg) stvec
+        );
+    }
+
+    println!("stvec is set to: 0x{:016x}", stvec);
+
+
     // leave
     Ok(())
 }
 
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct TrapFrame {
+    pub regs: [usize; 32],  // 0 - 255
+    pub fregs: [usize; 32], // 256 - 511
+    pub pc: usize,          // 512
+}
+
 #[no_mangle]
-pub extern "C" fn rust_trap_handler() {
-    log::info!("trapped!");
+pub extern "C" fn rust_trap_handler(
+    sepc: usize,           // a0
+    stval: usize,          // a1
+    scause: usize,         // a2
+    sstatus: usize,        // a3
+    frame: *mut TrapFrame, // a4
+) -> usize {
+    println!("<--------- trap --------->");
+    println!("sepc: 0x{:016x}", sepc,);
+    println!("stval: 0x{:016x}", stval,);
+    println!("scause: 0x{:016x}", scause,);
+    println!("sstatus: 0x{:016x}", sstatus,);
+    let is_async = scause >> 63 & 1 == 1;
+    let cause_code = scause & 0xfff;
+    if is_async {
+        match cause_code {
+            5 => {
+                //log::info!("M mode timer triggered");
+                println!("vm timer interrupt triggered");
+                //clear_timers();
+                let call = sbi_call(EXTENSION_TIMER, 0x0, 0xdead, 0xbeef);
+                println!("Sbi call error: {},  value: {}", call.error, call.value);
+                assert_eq!(call.error, 0);
+                
+            }
+            _ => {
+                unimplemented!("Unknown M-mode interrupt id: {}", cause_code);
+            }
+        }
+    } else {
+        match cause_code {
+            _ => {
+                unimplemented!("Unknown M-mode Exception id: {}", cause_code);
+            }
+        }
+    }
+    sepc
 }
