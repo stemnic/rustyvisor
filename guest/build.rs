57fd47383f975c05e51ddeacf967f3393265da0e
Zhouqi Jiang <luojia@hust.edu.cn>
2024-01-08 14:37:22 +0800

    Rearrange project to build under one workspace
    Move linker script into build.rs
    
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- guest/build.rs
+++ guest/build.rs
@@ -1,4 +1,17 @@
-OUTPUT_ARCH("riscv")
+use std::{env, path::PathBuf};
+
+fn main() {
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let ld = &out.join("linker.ld");
+
+    std::fs::write(ld, LINKER_SCRIPT).unwrap();
+
+    println!("cargo:rustc-link-arg=-T{}", ld.display());
+    println!("cargo:rustc-link-search={}", out.display());
+}
+
+const LINKER_SCRIPT: &[u8] = b"
+OUTPUT_ARCH(riscv)
 
 ENTRY(entrypoint)
 
@@ -33,4 +46,4 @@ SECTIONS
         *(.bss .bss.*);
         PROVIDE(_elf_end = .);
     }  
-}
+}";
