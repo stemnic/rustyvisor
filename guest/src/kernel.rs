a167ea0ec9b3426f414ee622d00b469ae314dbd9
Zhouqi Jiang <luojia@hust.edu.cn>
2024-01-08 15:24:50 +0800

    Simplify guest design with sbi-rt crate
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- guest/src/kernel.rs
+++ guest/src/kernel.rs
@@ -23,7 +23,7 @@ pub extern "C" fn rust_entrypoint() -> ! {
 
     setup_vm();
 
-    let call = sbi_call(EXTENSION_TIMER, 0x0, 0xdead, 0xbeef);
+    let call = sbi_rt::set_timer(0xdeadbeef);
     println!("Sbi call error: {},  value: {}", call.error, call.value);
     assert_eq!(call.error, 0);
     
@@ -150,25 +150,6 @@ fn clear_timers() {
     }
 }
 
-fn sbi_call(extension: usize, function: usize, arg0: usize, arg1: usize) -> SbiRet {
-    let (error, value);
-    match () {
-        #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
-        () => unsafe { asm!(
-            "ecall",
-            in("a0") arg0, in("a1") arg1,
-            in("a6") function, in("a7") extension,
-            lateout("a0") error, lateout("a1") value,
-        ) },
-        #[cfg(not(any(target_arch = "riscv32", target_arch = "riscv64")))]
-        () => {
-            drop((extension, function, arg0, arg1));
-            unimplemented!("not RISC-V instruction set architecture")
-        }
-    };
-    SbiRet { error, value }
-}
-
 pub fn init() -> Result<(), Error> {
     // init UART
     uart::Uart::new(memlayout::UART_BASE).init();
@@ -223,7 +204,7 @@ pub extern "C" fn rust_trap_handler(
                 //log::info!("M mode timer triggered");
                 println!("vm timer interrupt triggered");
                 //clear_timers();
-                let call = sbi_call(EXTENSION_TIMER, 0x0, 0xdead, 0xbeef);
+                let call = sbi_rt::set_timer(0xdeadbeef);
                 println!("Sbi call error: {},  value: {}", call.error, call.value);
                 assert_eq!(call.error, 0);
                 
